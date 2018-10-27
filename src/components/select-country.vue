700d64ff7bc09815cd93a33a4265d5c8de43257f
cangulo <cangulo@nubixar.com>
2018-10-27 16:05:23 -0500

    select country

---
+++ src/components/select-country.vue
@@ -0,0 +1,16 @@
+<template>
+  <select v-model="value">
+    <option>Colombia</option>
+    <option>Aregentina</option>
+  </select>
+</template>
+<script>
+export default {
+  props: ['value'],
+  name: 'select-contry',
+  data () {
+    return {
+    }
+  }
+}
+</script>
